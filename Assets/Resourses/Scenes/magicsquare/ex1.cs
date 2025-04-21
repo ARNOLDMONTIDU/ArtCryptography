44e63564e8bef8639ad38d53dfabafab5e2e79d4
dodik2281337322 <art.nork2003@gmail.com>
2025-04-21 21:59:24 +0700

    qwe

--- Assets/Resourses/Scenes/magicsquare/ex1.cs
+++ Assets/Resourses/Scenes/magicsquare/ex1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
+using System;
 
 public class ex1 : MonoBehaviour
 {
@@ -11,10 +13,19 @@ public class ex1 : MonoBehaviour
     public Button btnClick;
     [SerializeField] TMP_InputField inputField; 
     [SerializeField] TextMeshProUGUI resultText;
+    public GameObject[] objects = new GameObject[16];
+    
+    string phrase = "¿√≈Õ“ œ–Œ¬¿À»À—ﬂ";
 
     private void Start()
     {
-             
+        phrase = "¿√≈Õ“ œ–Œ¬¿À»À—ﬂ";
+        string resphrase = phrase.Trim();
+        for (int i = 0; i < objects.Length; i++)
+        {
+            string istring = Convert.ToString(i);
+            objects[i].GetComponent<Text>().text = istring;
+        }
     }
 
     public void ValidateInput()
